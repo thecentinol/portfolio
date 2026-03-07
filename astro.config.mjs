9aaca71c89886e602ca9e96a77280bce64f459ba
thecentinol <centinol@proton.me>
2026-03-07 11:43:51 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	output: "server",
+});
