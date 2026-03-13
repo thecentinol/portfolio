89dcf463261f3c7553642a92103b8393a958db94
thecentinol <centinol@proton.me>
2026-03-13 23:26:41 +0200

    build: upgrade astro, change output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,28 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
-	experimental: {
-		fonts: [
-			{
-				provider: fontProviders.google(),
-				name: "Syne",
-				weights: ["400", "700", "800"],
-				cssVariable: "--font-syne",
-			},
-			{
-				provider: fontProviders.google(),
-				name: "DM Sans",
-				weights: ["200", "400", "700"],
-				cssVariable: "--font-dmsans",
-			},
-			{
-				provider: fontProviders.google(),
-				name: "IBM Plex Mono",
-				weights: ["300"],
-				cssVariable: "--font-ibm",
-			},
-		],
-	},
+	fonts: [
+		{
+			provider: fontProviders.google(),
+			name: "Syne",
+			weights: ["400", "700", "800"],
+			cssVariable: "--font-syne",
+		},
+		{
+			provider: fontProviders.google(),
+			name: "DM Sans",
+			weights: ["200", "400", "700"],
+			cssVariable: "--font-dmsans",
+		},
+		{
+			provider: fontProviders.google(),
+			name: "IBM Plex Mono",
+			weights: ["300"],
+			cssVariable: "--font-ibm",
+		},
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},
-	output: "server",
+	output: "static",
 });
